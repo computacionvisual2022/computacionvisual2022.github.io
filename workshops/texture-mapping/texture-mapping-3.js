75db758506cfba66fb09e45625a4b4fc0008e975
DavidLeon93 <DavidLeon93@users.noreply.github.com>
2022-05-23 03:24:48 +0000

    deploy: 2c0fd0694c24df94e43830a30ab1021f98c6a0db

--- workshops/texture-mapping/texture-mapping-3.js
+++ workshops/texture-mapping/texture-mapping-3.js
@@ -1,51 +1,70 @@
-const ROWS = 20;
-const COLS = 20;
+new p5((p)=> {
+const ROWS = 25;
+const COLS = 25;
 const LENGTH = 20;
-let quadrille;
+const TLENGTH = 512;
+let quadrille, basetriangle;
 let row0, col0, row1, col1, row2, col2;
-let v0color, v1color, v2color;
 
-function setup() {
-  createCanvas(COLS * LENGTH, ROWS * LENGTH);
-  quadrille = createQuadrille(20, 20);
-  v0color = createColorPicker(color('red'));
-  v1color = createColorPicker(color('green'));
-  v2color = createColorPicker(color('blue'));
-  v0color.position(10, 10);
-  v1color.position(80, 10);
-  v2color.position(150, 10);
-  v0color.input(() => { quadrille.colorizeTriangle(row0, col0, row1, col1, row2, col2, v0color.color(), v1color.color(), v2color.color()) });
-  v1color.input(() => { quadrille.colorizeTriangle(row0, col0, row1, col1, row2, col2, v0color.color(), v1color.color(), v2color.color()) });
-  v2color.input(() => { quadrille.colorizeTriangle(row0, col0, row1, col1, row2, col2, v0color.color(), v1color.color(), v2color.color()) });
-  randomize();
-}
+let strip = 1;
+let texture;
 
-function draw() {
-  background('#060621');
-  drawQuadrille(quadrille, { cellLength: LENGTH, outlineWeight: 1, outline: 'green', board: true });
-  tri();
-}
+p.preload = () =>{
+  texture = p.loadImage('/workshops/texture-mapping/textures/grass.png');
+};
 
-function tri() {
-  push();
-  stroke('cyan');
-  strokeWeight(3);
-  noFill();
-  triangle(col0 * LENGTH + LENGTH / 2, row0 * LENGTH + LENGTH / 2, col1 * LENGTH + LENGTH / 2, row1 * LENGTH + LENGTH / 2, col2 * LENGTH + LENGTH / 2, row2 * LENGTH + LENGTH / 2);
-  pop();
-}
+p.setup = () => {
+  
+  p.createCanvas(500, 500);
+  p.pixelDensity(1);
+  p.randomize();
+};
 
-function keyPressed() {
-  randomize();
-}
+p.draw = () => {
+ 
+  basetriangle = p.createGraphics(p.width, p.height);
+  basetriangle.pixelDensity(1);
+  p.background('255');
 
-function randomize() {
-  row0 = int(random(0, ROWS));
-  col0 = int(random(0, COLS));
-  row1 = int(random(0, ROWS));
-  col1 = int(random(0, COLS));
-  row2 = int(random(0, ROWS));
-  col2 = int(random(0, COLS));
-  quadrille.clear();
-  quadrille.colorizeTriangle(row0, col0, row1, col1, row2, col2, v0color.color(), v1color.color(), v2color.color());
-}
+  /*p.image(texture, 0, 0, p.width, p.height);
+  p.loadPixels();
+
+  /*
+  for (let y = 0; y < p.height; y++){
+    for(let x = i * strip; x < p.width; x += strip){
+       if(let k = (x + y * p.width) * 4;
+       let red = p.pixels[k];
+       let green = p.pixels[k + 1];
+       let blue = p.pixels[k + 2];
+
+       for(let j = 0; j < strip; j++){
+          for(let alpha = 0; alpha <= 1; alpha += 0.1){
+            for(let beta = 0; beta <= 1; beta += 0.1){
+              for(let gamma = 0; gamma <= 1; gamma += 0.1){
+                if((x = (alpha*row0 + beta*row1 + gamma*row2)) and (y = (alpha*col0 + beta*col1 + gamma*col2)) and ((alpha + beta + gamma) = 1 ))
+                basetriangle.set(x + j, y, p.color(red, green, blue));
+              }
+            }
+          }
+       } 
+     }    
+  }
+
+  p.updatePixels();
+  basetriangle.updatePixels();*/
+};
+
+p.keyPressed = () => {
+  p.randomize();
+};
+
+p.randomize = () => {
+  row0 = p.int(p.random(0, texture.width));
+  col0 = p.int(p.random(0, texture.height));
+  row1 = p.int(p.random(0, texture.width));
+  col1 = p.int(p.random(0, texture.height));
+  row2 = p.int(p.random(0, texture.width));
+  col2 = p.int(p.random(0, texture.height));
+  p.triangle(row0, col0, row1, col1, row2, col2);
+};
+}, "texture-mapping-3");
