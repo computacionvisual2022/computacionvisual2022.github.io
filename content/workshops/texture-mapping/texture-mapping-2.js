f3122675d6f7df749de43a9f3a4fe0738a1df457
Miguel Medellín <mamedellinr@unal.edu.co>
2022-05-22 20:02:53 -0500

    update reports

--- content/workshops/texture-mapping/texture-mapping-2.js
+++ content/workshops/texture-mapping/texture-mapping-2.js
@@ -1,33 +1,100 @@
-let texture1;
+let texture1, texture2, texture3, texture4, textureSelect, textureWrapSelect;
+let sliderX, sliderY;
+let textureWrapValues, textureWrapOptions = ['CLAMP', 'MIRROR', 'REPEAT'];
+
 function preload() {
    texture1 = loadImage('/workshops/texture-mapping/textures/dirt.png');
+   texture2 = loadImage('/workshops/texture-mapping/textures/grass.png');
+   texture3 = loadImage('/workshops/texture-mapping/textures/pavement.png');
+   texture4 = loadImage('/workshops/texture-mapping/textures/sand.png');
 }
 
 function setup() {
-   createCanvas(100, 100, WEBGL);
-   textureWrap(MIRROR);
+   textureWrapValues = [CLAMP, MIRROR, REPEAT];
+
+   sliderXIndicator = createDiv('Eje X');
+   sliderXIndicator.position(15, 10);
+
+   sliderYIndicator = createDiv('Eje Y');
+   sliderYIndicator.position(15, 35);
+
+   sliderX = createSlider(0, 4096, 512);
+   sliderX.position(60, 10);
+   sliderX.style('width', '430px');
+
+   sliderY = createSlider(0, 4096, 512);
+   sliderY.position(60, 35);
+   sliderY.style('width', '430px');
+
+   textureIndicator = createDiv('Textura:');
+   textureIndicator.position(15, 465);
+
+   textureSelect = createSelect();
+   textureSelect.position(70, 465);
+   textureSelect.option('Tierra');
+   textureSelect.option('Hierba');
+   textureSelect.option('Adoquinado');
+   textureSelect.option('Arena');
+
+   textureWrapXIndicator = createDiv('Modo de ajuste X:');
+   textureWrapXIndicator.position(15, 485);
+   textureWrapXSelect = createSelect();
+   textureWrapXSelect.position(140, 485);
+   textureWrapXSelect.option('CLAMP');
+   textureWrapXSelect.option('REPEAT');
+   textureWrapXSelect.option('MIRROR');
+
+   textureWrapYIndicator = createDiv('Modo de ajuste Y:');
+   textureWrapYIndicator.position(230, 485);
+   textureWrapYSelect = createSelect();
+   textureWrapYSelect.position(350, 485);
+   textureWrapYSelect.option('CLAMP');
+   textureWrapYSelect.option('REPEAT');
+   textureWrapYSelect.option('MIRROR');
+
+   createCanvas(500, 500, WEBGL);
 }
 
 function draw() {
-   background(0);
+   background(210, 210, 207);
+   loadTextureWrap();
 
-   let dX = mouseX;
-   let dY = mouseY;
-
-   let u = lerp(1.0, 2.0, dX);
-   let v = lerp(1.0, 2.0, dY);
+   let u = lerp(1.0, 2.0, sliderX.value());
+   let v = lerp(1.0, 2.0, sliderY.value());
 
    scale(width / 2);
-
-   texture(texture1);
+   loadTexture();
 
    beginShape(TRIANGLES);
-   vertex(-1, -1, 0, 0, 0);
-   vertex(1, -1, 0, u, 0);
-   vertex(1, 1, 0, u, v);
+   vertex(-0.9, -0.2, 0, 0, 0);
+   vertex(0.5, -0.5, 0, u, 0);
+   vertex(0.8, 0.8, 0, u, v);
 
-   vertex(1, 1, 0, u, v);
-   vertex(-1, 1, 0, 0, v);
-   vertex(-1, -1, 0, 0, 0);
    endShape();
+}
+
+function loadTexture() {
+   switch (textureSelect.value()) {
+      case 'Hierba': texture(texture2); break;
+      case 'Adoquinado': texture(texture3); break;
+      case 'Arena': texture(texture4); break;
+      default: texture(texture1); break;
+   }
+}
+
+function loadTextureWrap() {
+   let isBreak = false;
+
+   for (let i = 0; i < textureWrapOptions.length; i++) {
+      for (let j = 0; j < textureWrapOptions.length; j++) {
+         if (textureWrapXSelect.value() == textureWrapOptions[i] &&
+            textureWrapYSelect.value() == textureWrapOptions[j]) {
+            textureWrap(textureWrapValues[i], textureWrapValues[j]);
+            isBreak = true;
+            break;
+         }
+
+         if (isBreak) { break; }
+      }
+   }
 }
