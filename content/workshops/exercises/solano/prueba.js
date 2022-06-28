ab4d4d0e8d3ea89683b2a6908d31f4a81fa00268
jdsolanos <jdsolanos@unal.edu.co>
2022-06-28 14:09:01 -0500

    updating exercise

---
+++ content/workshops/exercises/solano/prueba.js
@@ -0,0 +1,13 @@
+let img;
+
+function preload(){
+    img = loadImage('/workshops/exercises/solano/fire_breathing.jpg');
+}
+
+function setup(){
+    createCanvas(100, 100, WEBGL);
+}
+
+function draw(){
+    image(img,this.x,this.y);
+}
